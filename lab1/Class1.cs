cd9fd6b78c3c3037ac060b2c9281d546f2ab50a5
Oskar.Moneta <Oskar.Moneta@PK5-13>
2022-03-05 14:14:14 +0100

    Dodaj pliki projektów.

---
+++ lab1/Class1.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace lab1
+{
+    class Ulamek
+    {
+        private int licznik;
+        private int mianownik;
+
+        public Ulamek()
+        {
+
+        }
+        public Ulamek(Ulamek previousUlamek)
+        {
+            Mianownik = previousUlamek.Mianownik;
+            Licznik = previousUlamek.Licznik;
+        }
+
+        public Ulamek(int mianownik, int licznik)
+        {
+            Mianownik = mianownik;
+            Licznik = licznik;
+        }
+
+        public int Mianownik { get; set; }
+
+        public int Licznik { get; set; }
+
+     
+
+        public override string ToString()
+        {
+            return "Licznik to " + Licznik + " a mianownik to " + Mianownik.ToString(); ;
+        }
+        
+    }
+}
---
+++ lab1/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace lab1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Ulamek ulamek1 = new Ulamek(10, 20);
+            Ulamek ulamek2 = new Ulamek(ulamek1);
+            
+            ulamek1.Mianownik = 39;
+            ulamek2.Mianownik = 41;
+
+            ulamek2.Licznik = 25;
+
+            Console.WriteLine(ulamek1.ToString());
+            Console.WriteLine(ulamek2.ToString());
+            
+            Console.ReadKey();
+        }
+    }
+}
